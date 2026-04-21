fafd153bbd584a395961b6f9e4457a1844196713
Neil Vana <ran2511dy@gmail.com>
2026-04-21 14:10:52 -0600

    interfaces y servicios respecto a
    Clase reserva y horario - cambio de nombre de MyDbcontext a RestauranteDbContext

---
+++ ProyectoI/Servicios/Interfaces/IReserva.cs
@@ -0,0 +1,16 @@
+﻿using ProyectoI.Entidades;
+
+namespace ProyectoI.Servicios.Interfaces
+{
+    public interface IReserva
+    {
+        //Reads
+        public List<Reserva> GetAllReservas ();
+        public Reserva GetReservaById (int reservaId);
+        public Reserva GetReservaMesaId (int reservaMesaId);
+        //writes
+        public Reserva CreateReserva (Reserva reserva);
+        public Reserva UpgradeReserva (int reservaI, Reserva reserva);
+        public void DeleteReserva (int reservaId);
+    }
+}

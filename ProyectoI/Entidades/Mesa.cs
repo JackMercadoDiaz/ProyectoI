28744cf87d2377eec0e53ff7d9a4fa593056ab1c
Andrid1407 <andridmarinf14@gmail.com>
2026-04-20 19:48:14 -0600

    Creacion de clases
    Creé las clases Lista de Espera y Mesa

---
+++ ProyectoI/Entidades/Mesa.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoI.Entidades
+{
+    public class Mesa
+    {
+        public int id { get; set; }
+        public string numMesa { get; set; }
+        public int capacidad { get; set; }
+        public int zonaId { get; set; }
+        public bool estado { get; set; }
+
+    }
+}

be73978ad02e044ef40cf3c91206672763f2f722
KERG <kendalrg@outlook.es>
2026-04-20 19:48:53 -0600

    Clase Clientes y Factura

---
+++ ProyectoI/Entidades/Factura.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoI.Entidades
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+        public int  reservaId { get; set; }
+        public float monto { get; set; }
+        public string fechaPago { get; set; }
+        public string estado { get; set; }
+    }
+}

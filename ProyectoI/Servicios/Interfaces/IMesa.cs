8c9d4eab4f971c3da2913d9ef36162dd4016f254
Andrid1407 <andridmarinf14@gmail.com>
2026-04-27 11:01:04 -0600

    Métodos creados
    Espero que estos sean suficientes, de no ser así, igualmente puedo venir a trabajar lo que me llegara a faltar

--- ProyectoI/Servicios/Interfaces/IMesa.cs
+++ ProyectoI/Servicios/Interfaces/IMesa.cs
@@ -3,11 +3,14 @@ namespace ProyectoI.Servicios.Interfaces
 {
     public interface IMesa
     {
+        // reads
         public List<Mesa> GetAllMesas();
         public Mesa GetMesaById(int id);
+        // writes
         public Mesa CreateMesa(Mesa mesa);
         public Mesa UpdateMesa(int id, Mesa mesa);
         void DeleteMesa(int id);
+        public Mesa CambiarEstadoMesa(int mesaId, bool estado);
 
     }
 }
